98fcfc482646b3b1afb76a3971dbadd6a3e1f371
virazoor <50130201+virazoor@users.noreply.github.com>
2024-05-01 11:01:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,14 @@
 import streamlit as st
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-
+import numpy as np
 import pandas as pd
-if uploaded_file is not None:
-    df = pd.read_csv(
-        uploaded_file,
-        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
-        engine='python',
-        na_values='-',
-        header=None)
 
-st.markdown('### アクセスログ（先頭5件）')
-st.write(df.head(5))
+st.title('Streamlit 超入門')
+st.write('DataFrame')
+
+df = pd.DataFrame(
+    np.random.rand(20, 3),
+    columns=['a', 'b', 'c']
+)
+st.area_chart(df)
+
 
